926491f1a412d6338fa9f95d4a3016943d745dd5
Joan Marie Verba <verba001@umn.edu>
2018-07-23 16:51:34 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyAyBIDf1uDRqVEmWzpoZXTaw_tTfuyik64",
-    authDomain: "directory-database-fcf9d.firebaseapp.com",
-    databaseURL: "https://directory-database-fcf9d.firebaseio.com",
-    projectId: "directory-database-fcf9d",
-    storageBucket: "directory-database-fcf9d.appspot.com",
-    messagingSenderId: "811437317690"
-};
